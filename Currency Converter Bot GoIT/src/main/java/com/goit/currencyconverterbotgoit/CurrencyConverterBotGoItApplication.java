ee995cdf47ec3f5c03a09a37955ae5b1ab021706
Sidroded <deinekindaniil21@gmail.com>
2023-04-07 18:58:19 +0200

    Initial commit

---
+++ Currency Converter Bot GoIT/src/main/java/com/goit/currencyconverterbotgoit/CurrencyConverterBotGoItApplication.java
@@ -0,0 +1,13 @@
+package com.goit.currencyconverterbotgoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConverterBotGoItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyConverterBotGoItApplication.class, args);
+    }
+
+}
---
+++ Currency Converter Bot GoIT/src/test/java/com/goit/currencyconverterbotgoit/CurrencyConverterBotGoItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goit.currencyconverterbotgoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterBotGoItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
