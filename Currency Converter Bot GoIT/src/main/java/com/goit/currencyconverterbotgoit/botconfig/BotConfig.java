5cd00e7abbcd6f1e51380468dd4ac6a5f136565d
Semenov Volodymyr <thesemenovv@gmail.com>
2023-04-12 17:04:20 +0300

    Create Telegram bot, it's can answer "I'm working"

--- Currency Converter Bot GoIT/src/main/java/com/goit/currencyconverterbotgoit/botconfig/BotConfig.java
+++ Currency Converter Bot GoIT/src/main/java/com/goit/currencyconverterbotgoit/botconfig/BotConfig.java
@@ -1,4 +1,25 @@
 package com.goit.currencyconverterbotgoit.botconfig;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@Data
+@PropertySource("application.properties")
 public class BotConfig {
+    @Value("${bot.name}")
+    String botName;
+
+    @Value("${bot.token}")
+    String token;
+
+    public String getBotName() {
+        return botName;
+    }
+
+    public String getToken() {
+        return token;
+    }
 }
