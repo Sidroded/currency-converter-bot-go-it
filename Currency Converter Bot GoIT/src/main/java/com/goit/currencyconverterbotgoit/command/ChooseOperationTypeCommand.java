b97908e29f80358f9c9579b7ee3933af0b22723b
APrudnik <aprudnik@gmail.com>
2023-04-17 19:27:29 +0300

    Update ChooseOperationTypeCommand.java
    edited ChooseOperationTypeCommand

--- Currency Converter Bot GoIT/src/main/java/com/goit/currencyconverterbotgoit/command/ChooseOperationTypeCommand.java
+++ Currency Converter Bot GoIT/src/main/java/com/goit/currencyconverterbotgoit/command/ChooseOperationTypeCommand.java
@@ -4,6 +4,7 @@ import com.goit.currencyconverterbotgoit.constant.ButtonId;
 import com.goit.currencyconverterbotgoit.constant.ButtonText;
 import com.goit.currencyconverterbotgoit.constant.MessageText;
 import com.goit.currencyconverterbotgoit.emoji.Emoji;
+import com.goit.currencyconverterbotgoit.user.BankType;
 import com.goit.currencyconverterbotgoit.user.OperationType;
 import com.goit.currencyconverterbotgoit.user.User;
 import com.goit.currencyconverterbotgoit.user.UserService;
@@ -39,19 +40,21 @@ public class ChooseOperationTypeCommand {
     private static InlineKeyboardMarkup setInclineKeyboardMarkupOfOperationType(User user){
         InlineKeyboardMarkup keyboardMarkup = new InlineKeyboardMarkup();
         List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
-
         List<InlineKeyboardButton> rowInLine = new ArrayList<>();
+
         var usdButton = new InlineKeyboardButton();
-        usdButton.setText(ButtonText.USD_CURRENCY_BUTTON_TEXT);
+        usdButton.setText(ButtonText.USD_CURRENCY_BUTTON_TEXT + getEmoji(OperationType.UAH_TO_USD,user).get());
         usdButton.setCallbackData(ButtonId.USD_CURRENCY_BUTTON.getId());
         rowInLine.add(usdButton);
+        keyboard.add(rowInLine);
+        rowInLine = new ArrayList<>();
 
         var eurButton = new InlineKeyboardButton();
-        eurButton.setText(ButtonText.EUR_CURRENCY_BUTTON_TEXT);
+        eurButton.setText(ButtonText.EUR_CURRENCY_BUTTON_TEXT + getEmoji(OperationType.UAH_TO_EUR,user).get());
         eurButton.setCallbackData(ButtonId.EUR_CURRENCY_BUTTON.getId());
         rowInLine.add(eurButton);
-
         keyboard.add(rowInLine);
+        rowInLine = new ArrayList<>();
 
         keyboardMarkup.setKeyboard(keyboard);
         return keyboardMarkup;
